ab9125daeb9ef6a0856fb116df7b6c41a4325257
Josh Clark <jclarktennis@gmail.com>
2020-03-21 18:43:42 -0400

    Got decorator with argument for function with argument to work.

--- animal.py
+++ animal.py
@@ -1,4 +1,4 @@
-
+from functools import wraps
 from random import choice, random
 
 
@@ -15,14 +15,46 @@ class Animal(object):
             - Maybe this is a good use case for a decorator?
     '''
 
-    def __init__(self, grid, x_pos, y_pos, speed):
+    def __init__(self, world, x_pos, y_pos, speed):
         """TODO: to be defined. """
-        self.grid = grid
-        self.location = grid.get_cell(x_pos, y_pos)
+        self._world = world
+        self._location = world.get_cell(x_pos, y_pos)
 
-        self.sex = 'M' if random() < 0.5 else 'F'
-        self.speed = speed
+        self._sex = 'M' if random() < 0.5 else 'F'
+        self._speed = speed
 
+        self._hunger = 100
+        self._energy = 100
+
+    @property
+    def location(self):
+        return self._location
+
+    @location.setter
+    def location(self, new_location):
+        self._location = new_location
+
+    @property
+    def energy(self):
+        return self._energy
+
+    @energy.setter
+    def energy(self, value):
+        self._energy = value
+
+    def spend_energy(cost):
+        def real_decorator(func):
+            def wrapper(*args):
+                func(args[0])
+                setattr(args[0], 'energy', getattr(args[0], 'energy') - cost)
+            return wrapper
+        return real_decorator
+
+    @spend_energy(10)
     def step(self):
         '''Take a step in a random direction.'''
         self.location = choice(self.location.get_adjacent_cells())
+
+    def move(self):
+        for i in range(self.speed):
+            self.step()
--- tests/test_animal.py
+++ tests/test_animal.py
@@ -4,10 +4,18 @@ from world import World
 
 def create_test_animal():
     world = World()
-    animal = Animal(world.grid, 3, 3, 1)
+    animal = Animal(world, 3, 3, 1)
     return animal
 
 
 def test_spend_energy():
     animal = create_test_animal()
-    pass
+    assert animal.energy == 100
+    animal.step()
+    assert animal.energy == 90
+
+    animal2 = create_test_animal()
+    animal2.speed = 2
+    assert animal2.energy == 100
+    animal2.move()
+    assert animal2.energy == 80
