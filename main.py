56c6451a5292d91092a1f4a7774b13f6967c3f3f
Josh Clark <jclarktennis@gmail.com>
2020-03-28 12:20:42 -0400

    The graphical representation works to track an animal.

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
 import tkinter as tk
 
+from animal import Animal
 from world import World
+
+grid_squares = {}
+
+
+def handle_click(animal):
+    global grid_squares
+    grid_squares[(animal.location.x_pos, animal.location.y_pos)].winfo_children()[0]['bg'] = 'light gray'
+    animal.move()
+    grid_squares[(animal.location.x_pos, animal.location.y_pos)].winfo_children()[0]['bg'] = 'red'
 
 
 def main():
+    global grid_squares
+
     world = World(10)
+    animal = Animal(world, 0, 0, 1)
 
     window = tk.Tk()
 
@@ -21,10 +34,12 @@ def main():
             label = tk.Label(master=frame, text=f"Row {i}\nColumn {j}")
             label.pack()
 
+            grid_squares[(i, j)] = frame
+
     grid_holder.pack()
 
     controls = tk.Frame(master=window)
-    button = tk.Button(master=controls, text='Click me!')
+    button = tk.Button(master=controls, text='Click me!', command=lambda: handle_click(animal))
     button.pack()
     controls.pack()
 
