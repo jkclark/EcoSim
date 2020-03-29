028dd509317add700c84f512c6b472ae92ff1be2
Josh Clark <jclarktennis@gmail.com>
2020-03-29 12:06:45 -0400

    Split UI into different function.

--- main.py
+++ main.py
@@ -13,15 +13,9 @@ def handle_click(animal):
     grid_squares[(animal.location.x_pos, animal.location.y_pos)].winfo_children()[0]['bg'] = 'red'
 
 
-def main():
-    global grid_squares
-
-    world = World(10)
-    animal = Animal(world, 0, 0, 1)
-
-    window = tk.Tk()
-
-    grid_holder = tk.Frame(master=window)
+def create_main_visuals(main_layout, world, animal):
+    center_frame = tk.Frame(master=main_layout)
+    grid_holder = tk.Frame(master=center_frame)
 
     for i in range(world.size):
         for j in range(world.size):
@@ -38,10 +32,35 @@ def main():
 
     grid_holder.pack()
 
-    controls = tk.Frame(master=window)
+    controls = tk.Frame(master=center_frame)
+    controls.pack()
     button = tk.Button(master=controls, text='Click me!', command=lambda: handle_click(animal))
     button.pack()
-    controls.pack()
+
+    center_frame.grid(row=0, column=0)
+
+
+def create_square_details(main_layout):
+    square_details = tk.Frame(master=main_layout)
+    square_details_label = tk.Label(master=square_details, text='Details for square')
+    square_details_label.pack()
+    square_details.grid(row=0, column=1)
+
+
+def main():
+    global grid_squares
+
+    world = World(10)
+    animal = Animal(world, 0, 0, 1)
+
+    window = tk.Tk()
+
+    main_layout = tk.Frame(master=window)
+
+    create_main_visuals(main_layout, world, animal)
+    create_square_details(main_layout)
+
+    main_layout.pack()
 
     window.mainloop()
 
